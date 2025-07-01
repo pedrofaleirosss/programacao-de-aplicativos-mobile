4d7f8bc647155db5ec36391666e6c1d10d6657a3
Pedro Faleiros <142272176+pedrofaleirosss@users.noreply.github.com>
2025-06-30 22:05:02 -0300

    adicionando appWebView

---
+++ appWebView/app/src/main/java/appwebview/controle/actPrincipal.java
@@ -0,0 +1,31 @@
+package appwebview.controle;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class actPrincipal extends AppCompatActivity {
+
+    WebView wvWeb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_act_principal);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        wvWeb = findViewById(R.id.wvWeb);
+        wvWeb.loadUrl("http://192.168.200.2/alunos/3DSA/Pedro%20Faleiros/pam/siteapp/");
+        wvWeb.getSettings().setJavaScriptEnabled(true);
+    }
+}
